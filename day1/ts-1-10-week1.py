6b5541c03d42b3a751de6be5765259c8dcd251ce
Janjao <153170628+Jedsadaviriya@users.noreply.github.com>
2026-07-01 20:22:04 +0200

    update ts 1-10
    forgot that I updated the var, now fixed it

--- day1/ts-1-10-week1.py
+++ day1/ts-1-10-week1.py
@@ -1,6 +1,6 @@
 # min to hour and min calculator
 minutes = int(input("How many minutes do you want to convert to hours?: "))
-mth = min / 60
+mth = minutes / 60
 # mthwhole = min // 60 # directly does the rounding, so if this were in use, the int in int(mth) wouldn't have been needed 
-mtm = min % 60
-print(f"{min} Minutes converts to {int(mth)} hour/s and {mtm} Minutes")
+mtm = minutes % 60
+print(f"{minutes} Minutes converts to {int(mth)} hour/s and {mtm} Minutes")
